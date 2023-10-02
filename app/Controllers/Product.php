75ddb2e6c0cc6c7c669d86d793c0900020382484
cukiprit <andhikadwikyp@gmail.com>
2023-10-02 17:01:05 +0700

    Second Commit

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -23,45 +23,12 @@ class Product extends BaseController
 
     public function create()
     {
-        $rules = [
-            'nama_barang' => 'required|min_length[3]|max_length[50]',
-            'deskripsi_barang' => 'required|min_length[3]|max_length[50]',
-            'harga_barang' => 'required|min_length[3]|max_length[50]',
-            'stok_barang' => 'required|min_length[3]|max_length[50]',
-            'gambar_barang' => 'uploaded[gambar_barang]|max_size[gambar_barang,2048]|is_image[gambar_barang]|mime_in[gambar_barang,image/jpg,image/jpeg,image/png]',
-        ];
-
-        if ($this->validate($rules)) {
-            $model = new ProductModel();
-
-            $data = [
-                'product_name' => $this->request->getVar('nama_barang'),
-                'product_description' => $this->request->getVar('deskripsi_barang'),
-                'product_price' => $this->request->getVar('harga_barang'),
-                'product_stock' => $this->request->getVar('stok_barang'),
-                'product_picture' => $this->request->getFile('gambar_barang')->getRandomName(),
-            ];
-
-            if ($model->save($data) === false) {
-                $errors = $model->errors();
-
-                $data = [
-                    'validation' => $errors,
-                    'products' => $model->findAll(),
-                ];
-                return redirect('admin/tambah_barang');
-            } else {
-
-                return redirect('admin/tambah_barang');
-            }
-        } else {
-            $model = new ProductModel();
-
-            $data = [
-                'validation' => $this->validator,
-                'products' => $model->findAll(),
-            ];
-            return redirect('admin/tambah_barang');
+        $file = $this->request->getFile('gambar_barang');
+        if ($file->isValid() && !$file->hasMoved()) {
+            $newName = $file->getRandomName();
+            $file->move(WRITEPATH . 'uploads', $newName);
         }
+
+        $data;
     }
 }

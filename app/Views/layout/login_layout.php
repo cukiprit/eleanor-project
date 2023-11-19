0a483e3b814ad051cf13c110629676b01480bc59
Naufal Zaul Karim <naufalzaulkarim@gmail.com>
2023-11-19 22:05:36 +0700

    update style login, signup

--- app/Views/layout/login_layout.php
+++ app/Views/layout/login_layout.php
@@ -6,10 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="<?= base_url('img/logo/logo.png') ?>" type="image/x-icon">
   <link rel="stylesheet" href="<?= base_url('bootstrap/css/bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('styles/global.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('styles/login_layout-style.css') ?>">
   <title>Eleanor Project</title>
 </head>
 
-<body class="bg-dark">
+<body class="">
 
   <?= $this->renderSection('content') ?>
 
